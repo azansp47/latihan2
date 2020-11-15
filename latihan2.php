17ae253e933b7304e6a1da5838a69e81b96d35de
unknown <azansp47@gmail.com>
2020-11-15 19:17:16 +0700

    latihan2

---
+++ latihan2.php
@@ -0,0 +1,36 @@
+<?php
+$bilangan_terkecil = min(0,4,2,30,-20,-11); // untuk menampilkan nilai terkecil
+// echo $bilangan_terkecil;
+$bilangan_terbesar = max(0,4,2,30,-20,-11); // untuk menampilkan nilai terbesar
+// echo $bilangan_terbesar;
+// $bilangan_positif = abs(-7);// untuk merubah bilangan negatif ke positif
+// echo $bilangan_positif;
+$akar = sqrt(25); // untuk mencari akar dari suatu bilangan
+// echo $akar;
+
+
+// ARITMATIKA
+
+// + Addition Sum ( penjumlahan )
+// - Subtraction (pengurangan)
+// * Multiplication (perkalian)
+// / Division (pembagian)
+// % Modulus (bil bulat sisa pembagian)
+
+// sum
+
+$bilangan1 = 12;
+$bilangan2 = 13;
+$sum = $bilangan1 + $bilangan2;
+// echo $sum;
+
+// modulus
+
+$bilangan1 = 25;
+$bilangan2 = 15;
+// $modulus = $bilangan1 % $bilangan2;// sisa bagi di belakang koma
+// echo $modulus;
+
+
+
+
